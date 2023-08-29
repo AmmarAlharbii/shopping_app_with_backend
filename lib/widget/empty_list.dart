2640e9b7de7e58b4509a325ac5a5f636c06bfdaf
ammar <d97shot@gmail.com>
2023-08-29 09:02:28 +0300

    feat: add rive animation when list is empty

--- lib/widget/empty_list.dart
+++ lib/widget/empty_list.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
-class EmptyList extends StatelessWidget {
+class EmptyList extends StatefulWidget {
   const EmptyList({super.key});
+
+  @override
+  State<EmptyList> createState() => _EmptyListState();
+}
+
+class _EmptyListState extends State<EmptyList> {
+  /// Controller for playback
+  late RiveAnimationController _controller;
+
+  /// Is the animation currently playing?
+  bool _isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = OneShotAnimation(
+      'bounce',
+      autoplay: false,
+      onStop: () => setState(() => _isPlaying = false),
+      onStart: () => setState(() => _isPlaying = true),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
       //if list empty
-      child: Text(
-        'Empty list',
-        style: Theme.of(context)
-            .textTheme
-            .headlineMedium!
-            .copyWith(color: Colors.white),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(
+              'Empty list',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineMedium!
+                  .copyWith(color: Colors.white),
+            ),
+            const RiveAnimation.asset(
+              useArtboardSize: true,
+              'assets/5825-11357-empty-bee.riv',
+            ),
+          ],
+        ),
       ),
     );
   }
